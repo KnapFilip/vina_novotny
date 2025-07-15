19f931c648b4e166de707cbec5e1772387ca81c5
FilipKnap <KnapFilip@email.cz>
2025-07-15 09:26:23 +0200

    Add initial files

---
+++ js/f12.js
@@ -0,0 +1,13 @@
+document.addEventListener("keydown", function (event) {
+    if (
+        event.key === "F12" ||
+        (event.ctrlKey && event.shiftKey && (event.key === "I" || event.key === "J")) ||
+        (event.ctrlKey && event.key === "U")
+    ) {
+        event.preventDefault();
+    }
+});
+
+document.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
